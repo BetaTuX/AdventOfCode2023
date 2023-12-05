2e2985614f9f121d38db5ea35a9dee05596ae6da
Argann Bay-Eymère <argann.bayeymere@gmail.com>
2023-12-05 02:44:37 +0100

    + Added day04 (part2) (((first day in seoul, jetlag?)))

--- day04/main.go
+++ day04/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"math"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
+
+type Card struct {
+	number, matchAmount, copies int
+}
 
 const (
 	inputFilename = "input.txt"
 )
 
 var (
-	fileLines []string
-	cardRegex = regexp.MustCompile(`(?m)Card\s+[0-9]+: ([0-9\s]+) \| ([0-9\s]+)$`)
+	useCardCopyRule bool
+	fileLines       []string
+	cardRegex       = regexp.MustCompile(`(?m)Card\s+([0-9]+): ([0-9\s]+) \| ([0-9\s]+)$`)
 )
+
+func init() {
+	flag.BoolVar(&useCardCopyRule, "match-wins-copy", false, "Every match on your card gives you an extra copy of the n next cards (where n is the amount of matches for your card)")
+	flag.Parse()
+}
 
 func evaluateCardPoints(nbOfMatches int) int {
 	if nbOfMatches <= 0 {
@@ -32,15 +44,19 @@ func init() {
 	fileLines = strings.Split(string(file[:]), "\n")
 }
 
-func parseCard(line string) int {
+func parseCard(line string) (int, int) {
 	var winningNumbers []string
 	var playedNumbers []string
 	match := 0
 
 	results := cardRegex.FindAllStringSubmatch(line, -1)[0]
-	winningNumbers = strings.Split(results[1], " ")
-	playedNumbers = strings.Split(results[2], " ")
+	cardNumber, conversionError := strconv.Atoi(results[1])
+	winningNumbers = strings.Split(results[2], " ")
+	playedNumbers = strings.Split(results[3], " ")
 
+	if conversionError != nil {
+		log.Panicf("error: card number (%s) couldn't be parsed:\n%v", results[1], conversionError)
+	}
 	for _, winningRef := range winningNumbers {
 		if winningRef == "" {
 			continue
@@ -51,20 +67,39 @@ func parseCard(line string) int {
 			}
 		}
 	}
-	return evaluateCardPoints(match)
+	return cardNumber, match
 }
 
 func main() {
-	cardResults := make([]int, 0, len(fileLines))
+	cards := make([]Card, 0, len(fileLines))
 
 	for _, line := range fileLines {
-		cardResults = append(cardResults, parseCard(line))
+		cardNumber, cardMatchAmount := parseCard(line)
+		cards = append(cards, Card{
+			number:      cardNumber,
+			matchAmount: cardMatchAmount,
+			copies:      1,
+		})
 	}
 
-	sum := 0
-	for _, result := range cardResults {
-		sum += result
+	pointTotal := 0
+	cardTotal := 0
+	for index, card := range cards {
+		pointTotal += evaluateCardPoints(card.matchAmount) * card.copies
+		if useCardCopyRule {
+			startIndex := index + 1
+			endIndex := int(math.Min(float64(startIndex+card.matchAmount), float64(len(cards))))
+
+			for i := range cards[startIndex:endIndex] {
+				cards[startIndex+i].copies += card.copies
+			}
+		}
+		cardTotal += card.copies
 	}
 
-	println("result :", sum)
+	if useCardCopyRule {
+		println("result :", cardTotal)
+	} else {
+		println("result :", pointTotal)
+	}
 }
