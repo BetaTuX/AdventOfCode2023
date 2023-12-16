9c1112ee7e1ce97d1dfed5489ae9416a8b622716
Argann Bay-Eymère <argann.bayeymere@gmail.com>
2023-12-16 11:37:10 +0800

    + Added day10 (part2)

--- day10/main.go
+++ day10/main.go
@@ -26,6 +26,7 @@ type PipeType rune
 
 const (
 	PIP_START PipeType = 'S'
+	PIP_EMPTY PipeType = '.'
 	PIP_VER   PipeType = '|'
 	PIP_HOR   PipeType = '-'
 	PIP_NTE   PipeType = 'L'
@@ -34,10 +35,19 @@ const (
 	PIP_STW   PipeType = '7'
 )
 
+type Color int8
+
+const (
+	COLOR_UNMARKED Color = 0
+	COLOR_RED      Color = 1
+	COLOR_BLUE     Color = -1
+)
+
 type Tile struct {
 	Type           PipeType
 	X, Y           int
 	TunnelProgress int
+	mark           Color
 }
 
 type TunnelMap struct {
@@ -45,6 +55,21 @@ type TunnelMap struct {
 	MapHeight   int
 	MapWidth    int
 	StartingPos *Tile
+}
+
+func (t Tile) To(direction Direction) (x, y int) {
+	switch direction {
+	case DIR_NORTH:
+		return t.X, t.Y - 1
+	case DIR_EAST:
+		return t.X + 1, t.Y
+	case DIR_SOUTH:
+		return t.X, t.Y + 1
+	case DIR_WEST:
+		return t.X - 1, t.Y
+	default:
+		return -1, -1
+	}
 }
 
 // Returns next tile's coordinate according to direction (eg: tile(X:0, Y:0) -> go(DIR_NORTH) would return (0, -1))
@@ -85,18 +110,8 @@ func (t Tile) Go(currentDirection Direction) (x, y int, newDirection Direction) 
 		}
 	}
 
-	switch direction {
-	case DIR_NORTH:
-		return t.X, t.Y - 1, direction
-	case DIR_EAST:
-		return t.X + 1, t.Y, direction
-	case DIR_SOUTH:
-		return t.X, t.Y + 1, direction
-	case DIR_WEST:
-		return t.X - 1, t.Y, direction
-	default:
-		return -1, -1, direction
-	}
+	x, y = t.To(direction)
+	return x, y, direction
 }
 
 // Returns tile pointer, nil if doesn't exist (out of bound)
@@ -148,24 +163,35 @@ func (m TunnelMap) findNeighboors(tile *Tile) []*Tile {
 	}
 }
 
-func stepNavigationForward(m *TunnelMap, t **Tile, headingDirection *Direction) (bool, int) {
-	initialProgress := (*t).TunnelProgress
+func stepNavigationForward(m *TunnelMap, t **Tile, headingDirection *Direction, execOnTile func(*Tile) (bool, int)) (bool, int) {
 	x, y, dir := (*t).Go(*headingDirection)
 
 	*t = m.tileAt(x, y)
 	*headingDirection = dir
 
-	if (*t).TunnelProgress < 0 {
-		(*t).TunnelProgress = initialProgress + 1
-		return false, (*t).TunnelProgress
-	}
-	return true, initialProgress + 1
+	return execOnTile(*t)
 }
 
-func (m TunnelMap) navigate() (int, error) {
-	m.StartingPos.TunnelProgress = 0
+func (m *TunnelMap) identifyForward() (*Tile, Direction, []*Tile) {
 	neighboors := m.findNeighboors(m.StartingPos)
 	forwardDirection := Direction(slices.IndexFunc(neighboors, func(elem *Tile) bool { return elem != nil }))
+
+	return neighboors[forwardDirection], forwardDirection, neighboors
+}
+
+func (m *TunnelMap) navigate() (int, error) {
+	m.StartingPos.TunnelProgress = 0
+	forwardTile, forwardDirection, neighboors := m.identifyForward()
+
+	tilePathProgress := func(actualProgress int) func(*Tile) (bool, int) {
+		return func(t *Tile) (bool, int) {
+			if (*t).TunnelProgress < 0 {
+				(*t).TunnelProgress = actualProgress + 1
+				return false, (*t).TunnelProgress
+			}
+			return true, (*t).TunnelProgress
+		}
+	}
 
 	if forwardDirection < 0 {
 		return -1, fmt.Errorf("starting tile as no connection")
@@ -176,21 +202,74 @@ func (m TunnelMap) navigate() (int, error) {
 		return -1, fmt.Errorf("starting tile as only one connection")
 	}
 
-	forwardTile := neighboors[forwardDirection]
 	backwardTile := neighboors[backwardDirection]
 	pathProgress := 1
 
 	forwardTile.TunnelProgress = pathProgress
 	backwardTile.TunnelProgress = pathProgress
 	for ; forwardTile != backwardTile; pathProgress++ {
-		if looped, progress := stepNavigationForward(&m, &forwardTile, &forwardDirection); looped {
+		if looped, progress := stepNavigationForward(m, &forwardTile, &forwardDirection, tilePathProgress(pathProgress)); looped {
 			return progress, nil
 		}
-		if looped, progress := stepNavigationForward(&m, &backwardTile, &backwardDirection); looped {
+		if looped, progress := stepNavigationForward(m, &backwardTile, &backwardDirection, tilePathProgress(pathProgress)); looped {
 			return progress, nil
 		}
 	}
 	return pathProgress, nil
+}
+
+func (m *TunnelMap) identifyStartTileType() PipeType {
+	neighboors := m.findNeighboors(m.StartingPos)
+
+	switch {
+	case neighboors[0] == neighboors[2] && neighboors[2] == nil:
+		return PIP_HOR
+	case neighboors[1] == neighboors[3] && neighboors[3] == nil:
+		return PIP_VER
+	case neighboors[0] == neighboors[1] && neighboors[1] == nil:
+		return PIP_STW
+	case neighboors[0] == neighboors[3] && neighboors[3] == nil:
+		return PIP_STE
+	case neighboors[2] == neighboors[3] && neighboors[3] == nil:
+		return PIP_NTE
+	case neighboors[2] == neighboors[1] && neighboors[1] == nil:
+		return PIP_NTW
+	default:
+		return PIP_EMPTY
+	}
+}
+
+func (m *TunnelMap) markZones() Color {
+	lastFromTop := false
+	isInside := false
+
+	for tileIndex := range m.Map {
+		currentTile := &m.Map[tileIndex]
+
+		if tileIndex%m.MapWidth == 0 {
+			isInside = false
+		}
+		if currentTile.TunnelProgress < 0 {
+			if isInside {
+				currentTile.mark = COLOR_RED
+			} else {
+				currentTile.mark = COLOR_BLUE
+			}
+		} else {
+			currentType := currentTile.Type
+
+			if currentType == PIP_START {
+				currentType = m.identifyStartTileType()
+			}
+			if currentType == PIP_HOR ||
+				(lastFromTop && (currentType == PIP_STE || currentType == PIP_STW)) ||
+				(!lastFromTop && (currentType == PIP_NTE || currentType == PIP_NTW)) {
+				continue
+			}
+			isInside = !isInside
+		}
+	}
+	return COLOR_RED
 }
 
 func initTunnelMap() (TunnelMap, error) {
@@ -221,6 +300,7 @@ func initTunnelMap() (TunnelMap, error) {
 				Y:              y,
 				Type:           PipeType(tile),
 				TunnelProgress: -1,
+				mark:           COLOR_UNMARKED,
 			}
 			if tile == rune(PIP_START) {
 				tunnelMap.StartingPos = &tunnelMap.Map[y*mapWidth+x]
@@ -241,9 +321,32 @@ func main() {
 	}
 
 	furthestTileDistance, error := tunnelMap.navigate()
+	identifiedColor := tunnelMap.markZones()
+
+	enclosedTiles := 0
+
+	for index, tile := range tunnelMap.Map {
+		switch tile.mark {
+		case COLOR_UNMARKED:
+			print("X")
+		case COLOR_RED:
+			print("r")
+			if identifiedColor == COLOR_RED {
+				enclosedTiles++
+			}
+		case COLOR_BLUE:
+			print("b")
+			if identifiedColor == COLOR_BLUE {
+				enclosedTiles++
+			}
+		}
+		if (index+1)%tunnelMap.MapWidth == 0 {
+			println("")
+		}
+	}
 
 	if error == nil {
-		fmt.Printf("result: %d\n", furthestTileDistance)
+		fmt.Printf("result: %d\nenclosed tiles: %d\n", furthestTileDistance, enclosedTiles)
 	} else {
 		fmt.Printf("An error occured: %v\n", error)
 	}
