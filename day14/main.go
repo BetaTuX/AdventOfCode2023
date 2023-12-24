26f74d6ab2569c3cdaba764ca85964764d374ecd
Argann Bay-Eymère <argann.bayeymere@gmail.com>
2023-12-24 09:21:21 +0800

    + Added day14 (part2)

--- day14/main.go
+++ day14/main.go
@@ -8,7 +8,8 @@ import (
 )
 
 const (
-	inputFilename = "input.txt"
+	inputFilename   = "input.txt"
+	DEFAULT_MAXLOOP = 1000000000
 )
 
 func rotateMatrix[T any](matrix [][]T) [][]T {
@@ -41,6 +42,31 @@ func RollBalls(a, b byte) bool {
 	return b == '.' && a == 'O'
 }
 
+func evaluateBallWeight(m [][]byte) int {
+	sum := 0
+	for _, line := range m {
+		for index, char := range line {
+			if char == 'O' {
+				// Use index in line (actually column) to determine ball score
+				sum += index + 1
+			}
+		}
+	}
+	return sum
+}
+
+func cleanRecurrenceMap(r map[[4]int]int) {
+	for tuple, occurence := range r {
+		if occurence <= 1 {
+			delete(r, tuple)
+		}
+	}
+}
+
+func findEndLoopValue(start, loopLength, end int) int {
+	return loopLength - (((end - start) / loopLength) % loopLength)
+}
+
 func main() {
 	file, openError := os.ReadFile(inputFilename)
 
@@ -49,23 +75,37 @@ func main() {
 	}
 	linesAsString := strings.Split(string(file[:]), "\n")
 	fileLines := make([][]byte, 0, len(linesAsString))
+	recurrenceMap := make(map[[4]int]int)
 
 	for _, line := range linesAsString {
 		fileLines = append(fileLines, []byte(line))
 	}
-	// Rotate Right
+	// Rotate Right (North on right)
 	rotateMatrix(fileLines)
 
-	sum := 0
-	for _, line := range fileLines {
-		// Roll balls to the end of line
-		BubbleSort(line, RollBalls)
-		for index, char := range line {
-			if char == 'O' {
-				// Use index in line (actually column) to determine ball score
-				sum += index + 1
+	loopLimit := DEFAULT_MAXLOOP
+
+	for i := 0; i < loopLimit; i++ {
+		sequenceTuple := [4]int{}
+
+		for direction := 0; direction < 4; direction++ {
+			for _, line := range fileLines {
+				// Roll balls to the end of line
+				BubbleSort(line, RollBalls)
 			}
+			rotateMatrix(fileLines)
+			evaluated := evaluateBallWeight(fileLines)
+			sequenceTuple[direction] = evaluated
+		}
+		recurrenceMap[sequenceTuple]++
+		if recurrenceMap[sequenceTuple] > 2 && loopLimit == DEFAULT_MAXLOOP {
+			// Remove every occurence that happens only once (not in the loop)
+			cleanRecurrenceMap(recurrenceMap)
+			// We are already in the loop so remove 1 to i to place cursor before the beginning of the loop
+			evaluatedSolutionIndex := findEndLoopValue(i-1, len(recurrenceMap), DEFAULT_MAXLOOP)
+			loopLimit = i + evaluatedSolutionIndex
 		}
 	}
+	sum := evaluateBallWeight(fileLines)
 	fmt.Printf("result: %d\n", sum)
 }
